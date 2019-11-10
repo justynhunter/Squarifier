19348a882a144be614d38a5dfee576dfa4dd58b0
Justyn Hunter <cjhunter@me.com>
2019-11-09 22:06:04 -0600

    add unit test project

---
+++ Squarifier.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Squarifier.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
